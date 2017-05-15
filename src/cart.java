f933faa7045ba05ed84d0f13278de55d34ec8dd4
user <mramirez@usa.com>
2017-05-15 15:41:59 -0500

    basic framework-methods

--- src/cart.java
+++ src/cart.java
@@ -8,7 +8,9 @@ public class cart {
 
 
         Scanner sc = new Scanner(System.in);
+        String anotherItem;
 
+    do {
         System.out.println("===== Menu =====");
 
         System.out.println("1.  Sale");
@@ -16,17 +18,58 @@ public class cart {
         System.out.println("3.  Exit");
 
         System.out.println("");
-        System.out.println("Select system option:  ");
 
+        System.out.println("Select system option:  ");
         int menuSelection = sc.nextInt();
 
+        System.out.println(" ");
+
+        if (menuSelection == 1) {
+            gotoItem();
+            //item method
+        } else if (menuSelection == 2) {
+            //goto receipt method
+            gotoReceipt();
+        } else if (menuSelection == 3) {
+            //exit method
+            gotoExit();
+        } else { // out of bounds method
+            gotoOutOfBounds();
+        }
+
+        sc.next(); // related to do you want to loop again/ purchase more items
+
+        System.out.println("Do you want to continue ?");
+        anotherItem = sc.next();
+
+    } while ( anotherItem.equalsIgnoreCase("y"));
+
+        //  meth--if select 1...do this send to items
+        //  meth if 2
+        // meth if 3
+        // if  error --then xxx
 
 
+    } // end main method
 
+    public static void gotoItem() {    // choose soda opt..return String
 
+        System.out.println( "Soda Options");
+        System.out.println("1. Coke ");
+        System.out.println("2. Pepsi ");
+        System.out.println("3. Big Red ");
 
-
+        // sout which soda you want?
+        // if any soda then method-->qty
 
     }
+    public static void gotoReceipt(){
 
-}
+    }
+    public static void gotoExit(){
+
+    }
+    public static void gotoOutOfBounds(){
+
+    }
+} // end of class
