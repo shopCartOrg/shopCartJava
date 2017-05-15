459a3899465cc8bf827d28906be4072302b31c8b
everardosifuentes <everardo.sifuentes@sbcglobal.net>
2017-05-15 14:27:46 -0500

    added initial menu and scanner.  added the option to enter either 1,2,3

---
+++ src/cart.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * Created by everardosifuentes on 5/15/17.
+ */
+public class cart {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== Menu =====");
+
+        System.out.println("1.  Sale");
+        System.out.println("2.  Print Receipt");
+        System.out.println("3.  Exit");
+
+        System.out.println("");
+        System.out.println("Select system option:  ");
+
+        int menuSelection = sc.nextInt();
+
+
+
+
+
+
+
+
+    }
+
+}
